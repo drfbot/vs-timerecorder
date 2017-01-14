2fe4f017f1600f37688fa194396f20abc1ade107
drfbot <zipfele@yahoo.com>
2017-01-14 17:15:56 +0100

    fusioncharts API + Stempeluhr

--- README.txt
+++ README.txt
@@ -1,12 +1,4 @@
-test -> Moe
-test -> Fabi
-test -> Baris
-test2 baris
-test -> esma
-
 
 nachzuinstallierende Module
 
 isotope-layout
-fusionstarts
-fusionmaps
--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,13 +1,17 @@
 /**
  * Created by zoosle on 13.01.17.
  */
+
+
+
+
 window.onload=function(){
 
     function action(){
         //$("#resulttablediv").append('<p> klasse! <p>')
     }
 
-}
+};
 
 
 
@@ -23,4 +27,34 @@ function somethingtoChartContainer() {
         dataFormat: "JSONURL",
         dataSource: "../data/data.json" //später dynamische JSON-Abfrage
     });
-};
+}
+
+function date_time(id)
+{
+    date = new Date;
+    year = date.getFullYear();
+    month = date.getMonth();
+    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'Jully', 'August', 'September', 'October', 'November', 'December');
+    d = date.getDate();
+    day = date.getDay();
+    days = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
+    h = date.getHours();
+    if(h<10)
+    {
+        h = "0"+h;
+    }
+    m = date.getMinutes();
+    if(m<10)
+    {
+        m = "0"+m;
+    }
+    s = date.getSeconds();
+    if(s<10)
+    {
+        s = "0"+s;
+    }
+    result = ''+days[day]+' '+months[month]+' '+d+' '+year+' '+h+':'+m+':'+s;
+    document.getElementById(id).innerHTML = result;
+    setTimeout('date_time("'+id+'");','1000');
+    return true;
+}
